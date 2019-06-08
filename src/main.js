478615f3075171a0b3fcf80605af3d67e6a8d970
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-06-07 23:01:17 -0400

    Import webcomponents loader directly from package - Delete unused files

--- src/main.js
+++
@@ -1,6 +0,0 @@
-
-import "@babel/polyfill";
-//import "@webcomponents/webcomponentsjs/bundles/webcomponents-sd-ce-pf";
-//import "webcomponents-loader";
-import "@webcomponents/webcomponentsjs/webcomponents-bundle";
-
--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,11 @@ module.exports = {
     devtool: 'source-map',
     entry: {
         'components': path.join(dirApp, 'components'),
-        'webcomponents-loader': path.join(dirApp, 'webcomponents-loader'),
-        'polyfills/webcomponents-sd-ce-pf': path.join('@webcomponents/webcomponentsjs/bundles', 'webcomponents-sd-ce-pf'),
-        'polyfills/webcomponents-sd-ce': path.join('@webcomponents/webcomponentsjs/bundles', 'webcomponents-sd-ce'),
-        'polyfills/webcomponents-sd': path.join('@webcomponents/webcomponentsjs/bundles', 'webcomponents-sd'),
-        'polyfills/webcomponents-ce': path.join('@webcomponents/webcomponentsjs/bundles', 'webcomponents-ce')
+        'webcomponents-loader': path.join('@webcomponents/webcomponentsjs', 'webcomponents-loader'),
+        'bundles/webcomponents-sd-ce-pf': path.join('@webcomponents/webcomponentsjs/bundles', 'webcomponents-sd-ce-pf'),
+        'bundles/webcomponents-sd-ce': path.join('@webcomponents/webcomponentsjs/bundles', 'webcomponents-sd-ce'),
+        'bundles/webcomponents-sd': path.join('@webcomponents/webcomponentsjs/bundles', 'webcomponents-sd'),
+        'bundles/webcomponents-ce': path.join('@webcomponents/webcomponentsjs/bundles', 'webcomponents-ce')
     },
     output: {
         path: path.join(__dirname, 'dist'),
