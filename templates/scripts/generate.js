0d9fb3a953ef60ca9a811338c36bbd0541c587b9
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-06-08 09:40:59 -0400

    Refine generator script. - Name files properly - Replace file contents with new name

--- templates/scripts/generate.js
+++ templates/scripts/generate.js
@@ -8,8 +8,14 @@ fs.mkdirSync(`${componentsPath}/${projectName}`);
 
 createDirectoryContents(templatePath, componentsPath, projectName);
 
+// Add the import to components.js
+fs.appendFile(`${componentsPath}/../components.js`, `import './components/${projectName}/${projectName}.js';\n`, function (err) {
+if (err) throw err;
+console.log('Saved!');
+});
 
-function createDirectoryContents (templatePath, componentsPath, newProjectPath) {
+
+function createDirectoryContents (templatePath, componentsPath, projectName) {
   const filesToCreate = fs.readdirSync(templatePath);
 
   filesToCreate.forEach(file => {
@@ -19,9 +25,9 @@ function createDirectoryContents (templatePath, componentsPath, newProjectPath) 
     const stats = fs.statSync(origFilePath);
 
     if (stats.isFile()) {
-      const contents = fs.readFileSync(origFilePath, 'utf8');
+      const contents = fs.readFileSync(origFilePath, 'utf8').replace(/gz-component/g, projectName);
       
-      const writePath = `${componentsPath}/${newProjectPath}/${file}`;
+      const writePath = `${componentsPath}/${projectName}/${file.replace('gz-component',projectName)}`;
       fs.writeFileSync(writePath, contents, 'utf8');
     }
   });
